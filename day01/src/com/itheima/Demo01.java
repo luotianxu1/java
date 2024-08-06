cf8dfe00a62126d14a04439fe038b3a33b4d8fcc
罗天旭 <1265062404@qq.com>
2024-08-06 10:39:52 +0800

    first commit

---
+++ day01/src/com/itheima/Demo01.java
@@ -0,0 +1,9 @@
+package com.itheima;
+
+public class Demo01 {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = 5.125;
+        System.out.println(a + b);
+    }
+}
